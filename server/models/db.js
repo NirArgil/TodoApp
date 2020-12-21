2a643c0b07b49d2e98290127ae8dd7caecc2919d
NIR <MAILMAIL10@GMAIL.COM>
2020-12-21 08:43:59 +0200

    heroku deployment

--- server/models/db.js
+++ server/models/db.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose")
 
-let dbUrl = 'mongodb://localhost:27017/todo-app.todos';
+let dbUrl = 'mongodb://localhost:27017/todo-app';
 
 if(process.env.db_url) {
   dbUrl = process.env.db_url;
 }
 
 
-mongoose.connect(dbUrl, {
-  // connecting to the mongodb database locally
-  
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+async function connect() {
+  await mongoose.connect(dbUrl, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  });
+}
+
 mongoose.set("debug", true) 
 mongoose.Promise = Promise 
 
@@ -20,3 +21,6 @@ mongoose.set('useCreateIndex', true);
 
 
 module.exports.Todo = require("./todo")
+module.exports = {
+  connect,
+};
